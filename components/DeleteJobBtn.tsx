bf055333a668b13e40446d38f7e610a2c995e7df
abdullahqayid <3boo0odq0v@gmail.com>
2024-03-01 23:40:37 +0300

    delete functionality successfuly done

--- components/DeleteJobBtn.tsx
+++ components/DeleteJobBtn.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { useToast } from "./ui/use-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Button } from "./ui/button";
+import { deleteJobAction } from "@/utils/actions";
 
-export default function DeleteJobBtn() {
-  return <h1 className="text-4xl">DeleteJobBtn</h1>;
+export default function DeleteJobBtn({ id }: { id: string }) {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+  const { mutate: DeleteJob, isPending } = useMutation({
+    mutationFn: (id: string) => deleteJobAction(id),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({ description: "there was an error" });
+        return;
+      }
+      queryClient.invalidateQueries({ queryKey: ["jobs"] });
+      queryClient.invalidateQueries({ queryKey: ["stats"] });
+      queryClient.invalidateQueries({ queryKey: ["charts"] });
+
+      toast({ description: "job removed" });
+    },
+  });
+
+  return (
+    <Button
+      size="sm"
+      disabled={isPending}
+      onClick={() => {
+        DeleteJob(id);
+      }}
+    >
+      {isPending ? "deleting..." : "delete"}
+    </Button>
+  );
 }
